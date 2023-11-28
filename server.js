4941a31bebec2500076115be35ff5db3fd309a6e
Bryan Hui <44713632+HippB@users.noreply.github.com>
2023-11-27 18:36:03 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const server = requrie("http").createSever(app);
+const server = require("http").createServer(app);
 
 app.use(express.static(path.join(__dirname+"/public")));
 
