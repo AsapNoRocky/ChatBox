18ce3727cb1fd26501e5b5431e4a9a12bd929e3f
AsapNoRocky <98507187+AsapNoRocky@users.noreply.github.com>
2023-11-27 18:47:56 -0800

    Update code.js

--- code.js
+++ code.js
@@ -18,4 +18,50 @@
 
     });
 
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
+        let message = app.querySelector(".chat-screen #message-input").value;
+        if(message.length == 0){
+            return;
+        }
+        renderMessage("my",{
+            username: uname,
+            text: message
+        });
+        socket.emit("chat",{
+            username: uname,
+            text: message
+        });
+        app.querySelector(".chat-screen #message-input").value = "";
+
+    });
+
+    function renderMessage(type,message){
+        let messageContainer = app.querySelector(".chat-screen .messages");
+        if(type == "my"){
+            let el = document.createElement("div");
+            el.setAttribute("class", "message my-message");
+            el.innerHTML =
+                <div>
+                    <div class="name">You</div>
+                    <div class="text">$(message.text)</div>
+                </div>
+                ;
+                messageContainer.appendChild(el);
+        } else if(type == "other"){
+            el.setAttribute("class", "message other-message");
+            el.innerHTML =
+                <div>
+                    <div class="name">$(message.username)</div>
+                    <div class="text">$(message.text)</div>
+                </div>
+                ;
+                messageContainer.appendChild(el);
+        } else if(type == "update"){
+            el.setAttribute("class", "update");
+            el.innerHTML = message;
+                messageContainer.appendChild(el);
+    }
+    // scroll chat to end
+    messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+
 })();
