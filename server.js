e862923206120693b2895ba7c2bf237e5b6974a9
AsapNoRocky <98507187+AsapNoRocky@users.noreply.github.com>
2023-11-27 18:51:15 -0800

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const server = require("http").createServer(app);
+const server = requrie("http").createSever(app);
+
+const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname+"/public")));
+
+io.on("connection", function(socket){
+    socket.on("newuser", function(username){
+        socket.broadcast.emit("update", username + "joined the chatroom");
+    });
+    socket.on("exituser", function(username){
+        socket.broadcast.emit("update", username + "left the chatroom");
+    });
+    socket.on("chat", function(message){
+        socket.broadcast.emit("chat", message);
+    });
+});
 
 server.listen(5000);
