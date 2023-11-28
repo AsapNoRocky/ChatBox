1b3c5844b0e68aef725655b269d56a6ecc807c35
AsapNoRocky <98507187+AsapNoRocky@users.noreply.github.com>
2023-11-27 20:22:30 -0800

    Update code.js

--- public/code.js
+++ public/code.js
@@ -1,81 +1,78 @@
-(function(){
-
+(function() {
     const app = document.querySelector(".app");
     const socket = io();
-
     let uname;
 
-    app.querySelector(".join-screen #join-user").addEventListener("click", function(){
+    app.querySelector(".join-screen #join-user").addEventListener("click", function() {
         let username = app.querySelector(".join-screen #username").value;
-        if(username.length == 0){
+        if (username.length === 0) {
             return;
         }
         socket.emit("newuser", username);
         uname = username;
         app.querySelector(".join-screen").classList.remove("active");
         app.querySelector(".chat-screen").classList.add("active");
-
-
     });
 
-    app.querySelector(".chat-screen #send-message").addEventListener("click", function(){
+    app.querySelector(".chat-screen #send-message").addEventListener("click", function() {
         let message = app.querySelector(".chat-screen #message-input").value;
-        if(message.length == 0){
+        if (message.length === 0) {
             return;
         }
-        renderMessage("my",{
+        renderMessage("my", {
             username: uname,
             text: message
         });
-        socket.emit("chat",{
+        socket.emit("chat", {
             username: uname,
             text: message
         });
         app.querySelector(".chat-screen #message-input").value = "";
-
     });
 
-        app.querySelector(".chat-screen #exit-chat").addEventListener("click", function(){
-            socket.emit("exituser",uname);
-            window.location.href = window.location.href;
-        });
+    app.querySelector(".chat-screen #exit-chat").addEventListener("click", function() {
+        socket.emit("exituser", uname);
+        app.querySelector(".join-screen").classList.add("active");
+        app.querySelector(".chat-screen").classList.remove("active");
+    });
 
-        socket.on("update", function(update){
-            renderMessage("update", update);
-        });
+    socket.on("update", function(update) {
+        renderMessage("update", update);
+    });
 
-        socket.on("chat", function(message){
-            renderMessage("other", message);
-        });
+    socket.on("chat", function(message) {
+        renderMessage("other", message);
+    });
 
-    function renderMessage(type,message){
+    function renderMessage(type, message) {
         let messageContainer = app.querySelector(".chat-screen .messages");
-        if(type == "my"){
-            let el = document.createElement("div");
+        let el = document.createElement("div");
+
+        if (type === "my") {
             el.setAttribute("class", "message my-message");
             el.innerHTML = `
                 <div>
                     <div class="name">You</div>
                     <div class="text">${message.text}</div>
                 </div>
-                `;
-                messageContainer.appendChild(el);
-        } else if(type == "other"){
+            `;
+        } else if (type === "other") {
             el.setAttribute("class", "message other-message");
-            el.innerHTML =`
+            el.innerHTML = `
                 <div>
                     <div class="name">${message.username}</div>
                     <div class="text">${message.text}</div>
                 </div>
-                `;
-                messageContainer.appendChild(el);
-        } else if(type == "update"){
+            `;
+        } else if (type === "update") {
             el.setAttribute("class", "update");
             el.innerHTML = `${message}`;
-                messageContainer.appendChild(el);
-    }
-    // scroll chat to end
-    messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
+        }
 
+        // Append the message to the container
+        messageContainer.appendChild(el);
+
+        // Scroll chat to end
+        messageContainer.scrollTop = messageContainer.scrollHeight - messageContainer.clientHeight;
     }
 })();
