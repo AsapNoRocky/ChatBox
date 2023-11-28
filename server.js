73a85f224ed49f5c71a9e6bb5298a515ac1c339d
AsapNoRocky <98507187+AsapNoRocky@users.noreply.github.com>
2023-11-27 17:37:36 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+
+const app = exprerss();
+const server = requrie("http").createSever(app);
+
+app.use(express.static(path.join(__dirname+"/public")));
+
+server.listen(5000);
